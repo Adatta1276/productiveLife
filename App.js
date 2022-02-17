9502839cd4e627a847ef1a4b4a821ccb068202bc
Adatta1276 <dattaayush08@gmail.com>
2022-02-17 21:25:27 +0530

    commit 1 (added files and basic card structure)

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+const ProgressCard = (props) => {
+  return (
+    <View style={{ alignItems: "center", justifyContent: "center" }}>
+      <View
+        style={{
+          backgroundColor: props.backgroundColor, //"#E46472"
+          width: props.cardWidth,
+          height: props.cardHeight,
+          borderRadius: props.borderRadius,
+        }}
+      ></View>
+    </View>
+  );
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <ProgressCard
+        backgroundColor={"#E46472"}
+        cardWidth={180}
+        cardHeight={200}
+        borderRadius={60}
+      />
     </View>
   );
 }
@@ -13,8 +33,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+/*import CircularProgress from "react-native-circular-progress-indicator";
+import { StyleSheet, Text, View } from "react-native";
+
+
+export default function App() {
+  return (
+    <View style={{ alignItems: "center", justifyContent: "center", flex: 1, backgroundColor:'#000' }}>
+      <CircularProgress
+        value={200}
+        radius={120}
+        duration={3000}
+        activeStrokeColor={"#2465FD"}
+        activeStrokeSecondaryColor={"#C25AFF"}
+        maxValue={200}
+      />
+    </View>
+  );
+}*/
---
+++ README.md
@@ -0,0 +1,1 @@
+# This is a to-do app built with React Native, designed with inspiration from Dribbble and Pinterest. Don't just fork, STAR it.
